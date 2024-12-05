dbf744d148a1b3128477ee62c6e748e76eaf2e8a
Krishna Singh <singhkrishna7481@gmail.com>
2024-12-05 12:16:57 +0530

    Initial commit

--- src/main/java/com/wellsfargo/counselor/services/UserService.java
+++ src/main/java/com/wellsfargo/counselor/services/UserService.java
@@ -1,5 +1,28 @@
 package com.wellsfargo.counselor.services;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wellsfargo.counselor.entity.User;
+import com.wellsfargo.counselor.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
